c5f91bf5f2b7a8a50efcbbd9b7dfa37ac8163eca
xellscript <talhaturhan00@gmail.com>
2025-04-07 21:16:40 +0300

    Temel Yapı

---
+++ admin/index.php
@@ -0,0 +1,1 @@
+Burası Login Sayfası
---
+++ sablon.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="XXXXXXXXX">
+
+
+<!-- CSS Files -->
+<link rel="stylesheet" href="./assets/css/boostrap.min.css">
+<link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+
+
+<script src="./assets/js/boostrap.bundle.min.js"></script>
+</body>
+</html>
